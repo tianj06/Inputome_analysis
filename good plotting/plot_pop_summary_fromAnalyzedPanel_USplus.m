c1a481cd06d91f6e0a62d02b47d9dac64cd25991
tianj06 <tianj06@gmail.com>
2015-09-24 17:09:58 -0400

    to make panel plotting of neurons from one input area
    mostly for presentation purpose. Include maxium 30 neurons in one slides

---
+++ good plotting/plot_pop_summary_fromAnalyzedPanel_USplus.m
@@ -0,0 +1,46 @@
+function plot_pop_summary_fromAnalyzedPanel_USplus(fl,savePath,figureName)
+    if nargin<3
+        figureName = 'temp';
+        if nargin<2
+            savePath = 'D:\Dropbox (Uchida Lab)\lab\FunInputome\rabies\allIdentified\Analysis\psthPlottings\';
+        end
+    end
+    psthAll = [];
+    auROCall = [];
+    for i = 1:length(fl)
+        load(fl{i},'analyzedData')
+        psthAll(i,:,:) = analyzedData.smoothPSTH;
+        auROCall(i,:,:) = analyzedData.rocPSTH;
+    end
+    
+    totalFile = length(fl);
+    colNum = 4;
+    rowNum = 4;
+    pagePicNum = colNum*rowNum;
+    % color for 90% water, 50% water, nothing, and airpuff; will modify later
+    colorSet = [0 	0 	255;%blue  
+                   30 	144 	255;%light blue  
+                   %128 	128 128; % grey
+                    0 0 0]/255; % black;
+    for n = 1:totalFile
+        load(fl{n},'lightResult')
+        pageIdx = floor((n-1)/pagePicNum)+1;
+        tempn = n-(pageIdx-1)*pagePicNum;
+
+        if (mod(n,pagePicNum)==1)
+            if pageIdx>1
+                set(gcf, 'Units', 'Inches', 'Position', [0, 0, 11, 8.5], 'PaperUnits', 'Inches', 'PaperSize', [11 8.5])
+                export_fig([savePath  figureName num2str(pageIdx-1) '.jpg'])
+            end
+            figure;
+            p = panel();
+            p.pack(rowNum,colNum)
+        end
+        colIdx = mod(tempn-1,colNum)+1;
+        rowIdx = floor((tempn-1)/colNum)+1;
+        p(rowIdx,colIdx).pack('v',2)
+        p(rowIdx,colIdx,1).select()
+    end
+    set(gcf, 'Units', 'Inches', 'Position', [0, 0, 10, 8], 'PaperUnits', 'Inches', 'PaperSize', [11 8.5])
+    export_fig([savePath  figureName num2str(pageIdx) '.jpg'])
+end
