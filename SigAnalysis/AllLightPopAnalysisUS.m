b1d34e1d82d7fa2fd6287a0516ca96b9a5634d1f
tianj06 <tianj06@gmail.com>
2015-10-27 14:19:44 -0400

    add bar graphs to pull all inputs areas and make some bar graphs

--- SigAnalysis/AllLightPopAnalysisUS.m
+++ SigAnalysis/AllLightPopAnalysisUS.m
@@ -10,7 +10,11 @@ for i = 1:length(fl)
     Tus(i,:) = CompuateUSrelatedResponse(fl{i});
     CS(i,:) = CompuateCSRelatedResponse(fl{i});    
 end
-
+bl = [];
+for i = 1:length(fl)
+    load(fl{i},'analyzedData')
+    bl(i) = mean(analyzedData.rawPSTH(1,1:1000));
+end
 %% merge some areas
 % PPTg: PPTg (all animals other than PPTg_an), PPTg_an('Laurel', 'Kittentail')
 % LH: LH_po ('Waterlily','Rice') LH_psth('Aubonpain') LH_an (all others)
@@ -32,7 +36,10 @@ for i = 1:length(oldnames)
     brainArea(idx) = newnames(i);
 end
 [G,areaName]=grp2idx(brainArea);
-
+for i = 1:length(areaName)
+    disp(areaName{i})
+    disp(sum(G==i))
+end
 %% seperate the latency by short and long
 llatency = nan(length(fl),1); % VTA neurons doesn't have latency are nans
 for i = 1:length(fl)
@@ -76,22 +83,121 @@ Tus_long = Tus(llatency>6|isnan(llatency),:);
 writetable(Tus_short,[savePath 'us_short2.txt'],'Delimiter',',');
 writetable(Tus_long,[savePath 'us_long2.txt'],'Delimiter',',');
 
+Tus_early = Tus(rdate<=11|isnan(llatency),:);
+Tus_late = Tus(rdate>11|isnan(llatency),:);
+writetable(Tus_early,[savePath 'us_early.txt'],'Delimiter',',');
+writetable(Tus_late,[savePath 'us_late.txt'],'Delimiter',',');
+
 % with CS and positive RPE
 Tus.RPE = Tus.sig50R&Tus.sigExp&Tus.RPEsign;
 Tus.RPEsign = Tus.RPEsign.*Tus.RPE;
 Tus.RPEsign(Tus.RPEsign==2) = -1;
 PosRPE = double(Tus.RPEsign.*CS.csValue >0);
 
+%% plot bar graphs of response
+us_plot = Tus(:,{'pureReward','pureRewardWithCue','pureExp'});
+us_plot.mixed = Tus.sig50Rvs50OM&(~Tus.pureReward);
+colNames = us_plot.Properties.VariableNames;
+percent = [];
+percent(1,:) = mean(us_plot{~isnan(llatency),:});
+percent(2,:) = mean(us_plot{llatency<=6,:});
+%percent(2,:) = mean(us_plot{llatency>6,:});
+percent(3,:) = mean(us_plot{rdate<=11,:});
+%percent(5,:) = mean(us_plot{rdate>11,:});
+
+%rowNames = {'short latency','long latency','all','early rabies','late rabies'};
+rowNames = {'all','short latency','early rabies'};
+figure;
+bar(100*percent)
+set(gca,'xticklabels',rowNames)
+legend(colNames)
+prettyP('','','','','a')
+
+figure;
+bar(100*percent')
+set(gca,'xticklabels',colNames)
+legend(rowNames)
+prettyP('','','','','a')
+%%
+clear percent
+percent(1,:) = mean(us_plot{llatency<=3,:});
+percent(2,:) = mean(us_plot{llatency>3&llatency<=5,:});
+percent(3,:) = mean(us_plot{llatency>5&llatency<=8,:});
+percent(4,:) = mean(us_plot{llatency>8,:});
+figure;
+bar(100*percent)
+legend(colNames)
+set(gca,'xticklabels',{'<3','3-5','5-8','>8'})
+savePath = 'C:\Users\uchidalab\Dropbox (Uchida Lab)\lab\FunInputome\rabies\analysis2015Fall\';
+plot_pop_summary_fromAnalyzedPanel(fl(llatency<=3&Tus.pureReward),savePath,'Inputs3msPureReward')
+
+plot_pop_summary_fromAnalyzedPanel(fl(llatency>3&llatency<=5&Tus.pureReward),savePath,'Inputs3-5msPureReward')
+
+plot_pop_summary_fromAnalyzedPanel(fl(llatency>5&llatency<=8&Tus.pureReward),savePath,'Inputs5-8msPureReward')
+
+figure;
+bins = 0:2:50;
+histData(1,:) = hist(bl(llatency<=3),bins);
+histData(2,:) = hist(bl(llatency>3&llatency<=5),bins);
+histData(3,:) = hist(bl(llatency>5&llatency<=8),bins);
+for i = 1:3
+    subplot(3,1,i)
+    histData(i,:) = histData(i,:)/sum(histData(i,:));
+    bar(bins,histData(i,:))
+    ylim([0 0.3])
+end
+
+percent(1,:) = mean(us_plot{llatency<=3&G~=6,:});
+percent(2,:) = mean(us_plot{llatency>3&llatency<=5&G~=6,:});
+percent(3,:) = mean(us_plot{llatency>5&llatency<=8&G~=6,:});
+percent(4,:) = mean(us_plot{llatency>8&G~=6,:});
+figure;
+bar(100*percent)
+legend(colNames)
+set(gca,'xticklabels',{'<3','3-5','5-8','>8'})
+
+subIdx = llatency>8;
+countA = zeros(1,length(areaName));
+for i = 1:length(areaName)
+    countA(i) = sum(G(subIdx)==i);
+end
 %% with CS and positive RPE, negative RPE
 
 AllRPE = double((Tus.RPEsign.*CS.csValue >0)&(Tus.OM50sign.*CS.csValue >0));
+RewardRPE = Tus.RPE;
 CSposNegRPE = AllRPE;
 CSposRPE = PosRPE;
-JoinedRPE = table(CSposRPE,CSposNegRPE);
+JoinedRPE = table(CSposRPE,CSposNegRPE,RewardRPE);
 JoinedRPE.brainArea = brainArea';
-writetable(JoinedRPE,[savePath 'jRPE2.txt'],'Delimiter',',');
+writetable(JoinedRPE,[savePath 'jRPE.txt'],'Delimiter',',');
 JoinedRPE_short = JoinedRPE(llatency<=6|isnan(llatency),:);
 writetable(JoinedRPE_short,[savePath 'jRPE_short2.txt'],'Delimiter',',');
+
+JoinedRPE_early = JoinedRPE(rdate<=11|isnan(llatency),:);
+writetable(JoinedRPE_early,[savePath 'jRPE_early.txt'],'Delimiter',',');
+
+JoinedRPE_late = JoinedRPE(rdate>11|isnan(llatency),:);
+writetable(JoinedRPE_late,[savePath 'jRPE_late.txt'],'Delimiter',',');
+
+%%
+rpe_plot = [Tus.RPE PosRPE AllRPE];
+percent = [];
+percent(1,:) = mean(rpe_plot(~isnan(llatency),:));
+percent(2,:) = mean(rpe_plot(llatency<=6,:));
+percent(3,:) = mean(rpe_plot(rdate<=11,:));
+
+rowNames = {'all','short latency','early rabies'};
+figure;
+w = [0.6 0.4 0.2];
+c = color_select(3);
+for i = 1:3
+    bar(1:3,100*percent(:,i),w(i),'FaceColor',c(i,:))
+    hold on;
+end
+set(gca,'xticklabels',rowNames)
+legend({'reward RPE', 'reward&cue RPE', 'all RPE'})
+prettyP('','','','','a')
+
 
 %% make average PSTH response for specific groups
 inputG = ~ismember(G,find(vtaAreaIdx));
