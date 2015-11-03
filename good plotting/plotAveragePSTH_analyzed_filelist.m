b35825cd5175fadacb2403eacc6bdce1593397bf
tianj06 <tianj06@gmail.com>
2015-11-03 09:38:45 -0500

    add an extra paramter to control whether free reward is plotted; return figures axes

--- good plotting/plotAveragePSTH_analyzed_filelist.m
+++ good plotting/plotAveragePSTH_analyzed_filelist.m
@@ -1,10 +1,14 @@
-function plotAveragePSTH_analyzed_filelist(fl)
+function ax = plotAveragePSTH_analyzed_filelist(fl,freeReward)
+if nargin <=1
+    freeReward = 0;
+end
+
 psth = zeros(length(fl),10,5001);
 k=1;
 for i = 1:length(fl)
     load(fl{i},'analyzedData')
-    analyzedData = remove_too_few_trials(analyzedData,10);
-    if ~isnan(analyzedData.smoothPSTH(5,1))
+    trialNum = cellfun(@(x)size(x,1),analyzedData.raster);
+    if (~isnan(analyzedData.smoothPSTH(9,1)))|(~freeReward)
         psth(k,:,:) = analyzedData.smoothPSTH(1:10,:);
         k = k+1;
     end
@@ -18,17 +22,24 @@ trialType = [1 2 7 9];
 
         
 figure;
-for i = 1:3
-        plot(-1:0.001:4,averagePSTH(i,:),'color',CueColor(i,:),'LineWidth',1.5)
+ax(1) = subplot(1,2,1);
+if freeReward
+    totalTrialTypes = 4;
+else
+    totalTrialTypes = 3;
+end 
+for i = 1:totalTrialTypes
+        plot(-1:0.001:4,averagePSTH(trialType(i),:),'color',CueColor(i,:),'LineWidth',1.5)
         hold on
 end
-legend('90% W','50% W','0% W','free W');
+h = legend('90% W','50% W','0% W','free W');
+set(h,'Location','NorthWest','box','off')
 prettyP([-0.9 3.8],'','','','l')
 xlabel('Time - Odor (ms)')
 ylabel('Spikes/s')
 title(['n=' num2str(k-1)])
 
-figure;
+ax(2) = subplot(1,2,2);
 for i = 1:3
         plot(-1:0.001:4,averagePSTH(i+4,:),'color',CueColor(i,:),'LineWidth',1.5)
         hold on
@@ -36,4 +47,5 @@ end
 prettyP([-0.9 3.8],'','','','l')
 xlabel('Time - Odor (ms)')
 ylabel('Spikes/s')
+set(gcf,'Position',[400   300   964   400])
 end
