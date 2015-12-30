09ef572e22e3bb485034d8f149a639336eb3e5eb
tianj06 <tianj06@gmail.com>
2015-12-29 20:18:05 -0500

    Ryan thesis analysis update

--- RyanThesisFigure.m
+++ RyanThesisFigure.m
@@ -1,4 +1,4 @@
-fl = what(pwd)
+fl = what(pwd);
 fl = fl.mat;
 % light identification
 formattedpath = 'C:\Users\uchidalab\Dropbox (Uchida Lab)\lab\Ryan thesis\PPTgAllunits';
@@ -8,12 +8,18 @@ plotflag = 1;
 [lightfiles, lightlatency,lightjitter]= ...
     SelectLightResponsiveUnits('PPTg',formattedpath,lowsalt, highsalt,0);
 
-finalLight = lightfiles(lightlatency<7);
+finalLight = lightfiles(lightlatency<6);
 lightLabel = ismember(fl,finalLight);
 % AAV vs Rabies label
 for i = 1:length(fl)
     animals{i} = extractAnimalFolderFromFormatted(fl{i});
 end
+%% remove cyrus
+cyrusLabel = ismember(animals,{'Cyrus'});
+animals = animals(~cyrusLabel);
+lightLabel = lightLabel(~cyrusLabel);
+fl = fl(~cyrusLabel);
+%%
 AAVlabel = ismember(animals,{'ATG','Bruno','Cyrus'});
 %% plotting sorted roc
 filelist = fl;
@@ -44,7 +50,7 @@ figure('Position',[200 200 1000 787]);
 subplot(1,6,1)
 b = zeros(length(AAVlight),4);
 b(AAVlight(plotorder),4) = 0.97;
-b(rabieslight(plotorder),4) = 0.6;
+b(rabieslight(plotorder),4) = 0.5;
 
 imagesc(1-b,[0 1])
 colormap(gca,'jet') 
@@ -64,4 +70,92 @@ for j = 1:5
     title(titleText{j});
 end
 
-% clustering all
+%% Principle component analysis
+proc = permute (rocPSTH(:,[1 2 7],10:40), [1,3,2]);
+N = length(fl);
+temp = squeeze(reshape(proc,N,1,[]));
+[eigvect,proj,eigval] = princomp(temp);
+nPC = 4;
+PCs = eigvect(:,1:nPC);
+PCs = reshape(PCs,[],3,nPC);
+colorset= [  0 	0 	255;%blue  
+             30 	144 	255;%light blue  
+             128 	128 128;]/255; % grey
+figure;
+for i = 1:nPC
+    subplot(1,nPC,i)
+    for j = 1:3
+        plot(squeeze(PCs(:,j,i)),'color',colorset(j,:))
+        hold on;
+    end
+    %set(gca,'xtick',[1:10:30],'xticklabel',{'0','1','2'})
+    title(sprintf('PC %d var: %0.1f%%', i,100*eigval(i)/sum(eigval)))
+    xlabel('Time - odor (s)')
+end
+
+% project neurons to first 3 PCs
+figure;
+subplot(2,1,1)
+scatter(proj(:,1),proj(:,2),'k')
+hold on;
+scatter(proj(AAVlight,1),proj(AAVlight,2),'Facecolor','b')
+scatter(proj(rabieslight,1),proj(rabieslight,2),'Facecolor','r')
+xlabel('PC1');ylabel('PC2');vline(0);hline(0);legend('all','AAV','rabies')
+
+
+subplot(2,1,2)
+scatter(proj(:,1),proj(:,3),'k')
+hold on;
+scatter(proj(AAVlight,1),proj(AAVlight,3),'Facecolor','b')
+scatter(proj(rabieslight,1),proj(rabieslight,3),'Facecolor','r')
+xlabel('PC1');ylabel('PC3');vline(0);hline(0);legend('all','AAV','rabies')
+%%
+% plot PC histogram
+bins = -3:0.2:3;
+figure;
+PCid = 2;
+p(:,1) = hist(proj(:,PCid),bins)/N;
+m(:,1) = median(proj(:,PCid));
+p(:,2) = hist(proj(AAVlight,PCid),bins)/sum(AAVlight);
+m(:,2) = median(proj(AAVlight,PCid));
+p(:,3) = hist(proj(rabieslight,PCid),bins)/sum(rabieslight);
+m(:,3) = median(proj(rabieslight,PCid));
+
+titles = {'all','AAV','rabies'};
+for i = 1:3
+    subplot(3,1,i)
+    bar(bins,p(:,i))
+    hold on; vline(m(i))
+    title(titles{i})
+    xlabel(['PC' num2str(PCid)])
+    ylabel('prob.')
+    xlim([-2 2])
+end
+% joint histogram PC1 and PC2
+%%
+edges{1} = -3:0.2:3;
+edges{2} = -2:0.2:2;
+figure;
+subplot(2,2,1)
+n1 = hist3(proj(:,[1,2]),'Edges',edges)/N;
+imagesc('XData',edges{1},'YData',edges{2},'CData',n1,[0 0.12])
+colormap(hot);xlim([-3 3]);ylim([-2 2])
+xlabel('PC1');ylabel('PC2');title('all')
+subplot(2,2,2)
+n1 = hist3(proj(AAVlight,[1,2]),'Edges',edges)/sum(AAVlight);
+imagesc('XData',edges{1},'YData',edges{2},'CData',n1,[0 0.12])
+colormap(hot);xlim([-3 3]);ylim([-2 2])
+xlabel('PC1');ylabel('PC2');title('AAV')
+subplot(2,2,3)
+n1 = hist3(proj(rabieslight,[1,2]),'Edges',edges)/sum(rabieslight);
+imagesc('XData',edges{1},'YData',edges{2},'CData',n1,[0 0.12])
+colormap(hot);xlim([-3 3]);ylim([-2 2])
+xlabel('PC1');ylabel('PC2');title('rabeis')
+%% plot mean response
+plotAveragePSTH_analyzed_filelist(fl(AAVlight))
+plotAveragePSTH_analyzed_filelist(fl(rabieslight))
+plotAveragePSTH_analyzed_filelist(fl)
+
+%5
+
+
